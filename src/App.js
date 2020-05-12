4849c5fe6a78035dc60ee50b679cf95ea1c48ada
Tshenolo17 <cse18-066@thuto.bac.ac.bw>
2020-05-13 00:31:37 +0200

    adding Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import styled from "styled-components";
 import "./App.css";
 import Header from "./navigationComponents/header/Header";
 import SideBar from "./navigationComponents/sidebar/SideBar";
 
+const MainContainer = styled.div`
+  display: flex;
+`;
+
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Route
+        path="/"
+        render={(props) => (
+          <Fragment>
+            <MainContainer>
+              <SideBar />
+              <Header />
+            </MainContainer>
+          </Fragment>
+        )}
+      />
+    </Router>
+  );
 }
 
 export default App;
--- src/navigationComponents/sidebar/SideBar.js
+++ src/navigationComponents/sidebar/SideBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import NavItem from "./NavItem";
+import { Row } from "simple-flexbox";
+import { Link } from "react-router-dom";
 import {
   BarGraphIcon,
   CreditIcon,
@@ -11,18 +12,89 @@ import {
 } from "../../contentComponents/icons/ProjectIcons";
 
 const NavContainer = styled.div`
-  width: 200px;
+  padding: 0 10px 0;
+  width: 250px;
   flex: 0 auto;
   height: 100%;
   min-height: 100vh;
   color: dark-grey;
-  background-color: (main-dark-blue);
+  background-color: var(--main-dark-blue);
+
+  .logo {
+    height: 60px;
+    background-color: orange;
+    margin-bottom: 50px;
+  }
+
+  .row {
+    margin-bottom: 7px;
+  }
+
+  .spacer {
+    height: 200px;
+  }
+`;
+
+const NavItemContainer = styled(Link)`
+  width: 100%;
+  font-size: 1em;
+  padding: 8px;
+
+  .text {
+    display: inline-block;
+    margin-left: 15px;
+    padding-top: 5px;
+    jusify: center;
+  }
 `;
 
 const SideBar = () => {
   return (
     <NavContainer>
-      <h1>sidebar</h1>
+      <div className="logo">Logo</div>
+      <Row className="row" horizontal="center" vertical="center">
+        <NavItemContainer to="/">
+          <BarGraphIcon />
+          <div className="text">DashBoard</div>
+        </NavItemContainer>
+      </Row>
+
+      <Row className="row" horizontal="center" vertical="center">
+        <NavItemContainer to="/">
+          <CreditIcon />
+          <div className="text">Calculate Credit</div>
+        </NavItemContainer>
+      </Row>
+
+      <Row className="row" horizontal="center" vertical="center">
+        <NavItemContainer to="/">
+          <LoanIcon />
+          <div className="text">Apply for Loan</div>
+        </NavItemContainer>
+      </Row>
+
+      <Row className="row" horizontal="center" vertical="center">
+        <NavItemContainer to="/">
+          <ReportIcon />
+          <div className="text">Credit Report</div>
+        </NavItemContainer>
+      </Row>
+
+      <Row className="row" horizontal="center" vertical="center">
+        <NavItemContainer to="/">
+          <InfoIcon />
+          <div className="text">Help</div>
+        </NavItemContainer>
+      </Row>
+
+      <div className="spacer"></div>
+
+      <Row className="row" horizontal="center" vertical="center">
+        <NavItemContainer to="/">
+          <SettingsIcon />
+          <div className="text">Settings</div>
+        </NavItemContainer>
+      </Row>
     </NavContainer>
   );
 };
